a7ffba9a9458b63a3e24e2ebce24127ddec8c757
Kosar-sh <k.shiri82@gmail.com>
2024-12-08 22:38:31 +0330

    Write more complex test

--- Lab03/sys_test.c
+++ Lab03/sys_test.c
@@ -2,13 +2,42 @@
 #include "syscall.h"
 #include "user.h"
 
+void test_process(int number)
+{
+    long long count = 0;
+    if (number != 3)
+    {
+        while (count != 10000000)
+        {
+            printf(1, "");
+            count++;
+        }
+    }
+    else if (number == 3)
+    {
+        set_process_parameter(getpid(), 100, 4);
+        change_queue(getpid(), 2);
+        print_process_info();
+    }
+}
+
 int main()
 {
     print_process_info();
-    set_process_parameter(getpid(), 100, 4);
-    change_queue(getpid(),2);
-    print_process_info();
+    int i;
+    for (i = 0; i < 4; i++)
+    {
+        if (fork() == 0)
+        {
+            test_process(i);
+            exit();
+        }
+    }
 
+    for (i = 0; i < 4; i++)
+    {
+        wait();
+    }
     exit();
     return 0;
 }
